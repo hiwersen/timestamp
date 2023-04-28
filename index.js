9d75b56fda7c58fdd7b2905cf68b8b2473514001
Hiwersen Angelo Gnocchi Godoy <100746220+hiwersen@users.noreply.github.com>
2023-04-28 10:14:48 +0100

    Comments reviewed fot suits JSDoc-style

--- index.js
+++ index.js
@@ -1,53 +1,78 @@
 require('dotenv').config();
-var express = require('express');
-var app = express();
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
 
-// enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
-// so that your API is remotely testable by FCC 
+/**
+ * Enable CORS (Cross-origin resource sharing) middleware
+ * so that your API is remotely testable by FreeCodeCamp
+ * @see {@link https://www.npmjs.com/package/cors}
+ */
 var cors = require('cors');
 app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
 
+/**
+ * Serve static files (CSS, JavaScript, images) from the 'public' directory
+ */
 app.use(express.static(__dirname + '/public'));
 
+/**
+ * Handle the root route and serves the index.html file.
+ */
 app.get("/", function (req, res) {
   res.sendFile(__dirname + '/views/index.html');
 });
 
+/**
+ * Handle the API route with an optional date parameter.
+ * @param {string} [date] - An optional date string or Unix timestamp.
+ */
 app.get("/api/:date?", (req, res) => {
   let { date } = req.params;
-  let isValideDate = !!Date.parse(date);
-  let isTimestampString = /^\d+$/.test(date);
-  let unix;
+
+  // Parse the date parameter from a string representing a date to a Unix timestamp {Number}
+  // @see {@link https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Date#the_epoch_timestamps_and_invalid_date}
+  // @see {@link https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Date/parse#date_time_string_format}
+  let parsedDate = Date.parse(date);
+
+  // Declare the unix and utc variables, which will be the keys for the JSON object returned in response to server requestes
+  let unix; 
   let utc;
 
+  // Check if the optional date value was passed in the route parameter
   if (!date) {
-    console.log("there is no date");
+    // Initialize unix and utc values as current time
     unix = Date.now();
-    utc = new Date();
-    utc = utc.toUTCString();
+    utc = new Date().toUTCString();
     res.json({ unix, utc });
 
-  } else if (!isValideDate) {
+    // Check if the date represents a valid format
+  } else if (!parsedDate) { 
+
+    // Check if it is a Unix timestamp {String} 
+    let isTimestampString = /^\d+$/.test(date);
     if(isTimestampString) {
-      console.log("it is a timestamp string");
+      // Initialize unix and utc values as input date
       unix = parseInt(date);
-      utc = new Date(unix);
-      utc = utc.toUTCString();
+      utc = new Date(unix).toUTCString();
       res.json({ unix, utc });
 
     } else {
-      console.log("there is a date, but the date is not valid");
-      res.json({ error: 'Invalide Date' });
+      res.json({ error: 'Invalid Date' });
     }
-  } else {
-    console.log("there is a valid date");
-    utc = new Date(date);
-    utc = utc.toUTCString();
-    unix = Date.parse(date);
+
+  }  else {
+    // Initialize unix and utc values as input date
+    unix = parsedDate;
+    utc = new Date(unix).toUTCString();
     res.json({ unix, utc });
   }
+
 });
 
-var listener = app.listen(process.env.PORT, function () {
+/**
+ * Starts the server and logs the listening port.
+ */
+var listener = app.listen(port, function () {
   console.log('Your app is listening on port ' + listener.address().port);
 });
